a9862e27cef529b207d9bc793d1a58f48eda8a80
ankien <andrew_kien12@yahoo.com>
2020-07-02 23:29:57 -0400

    bitwise operations are a nightmare

--- lzw.h
+++ lzw.h
@@ -4,6 +4,10 @@
 #include<fstream>
 #include<map>
 
+// Uses even/odd sequencing to store 12-bit codes in an 8-bit vector
+// Even: Index at 0, 2, 4, ...
+//  Odd: Index at 1, 3, 5, ...
+
 void encode(std::string& input, std::vector<unsigned char>& packedArray) {
     int16_t dictSize = 256;
     std::map<std::string,int16_t> dictionary; // only using 12 bits of int
@@ -11,67 +15,69 @@ void encode(std::string& input, std::vector<unsigned char>& packedArray) {
         dictionary[std::string(1,i)] = i;
     }
 
-    bool odd=true;
-    std::string w;
-    for(char& c : input) {
-        std::string wc = w + c;
-        if(dictionary.count(wc)) {
-            w = wc;
+    bool even=true;
+    std::string curr;
+    for(char& next : input) {
+        std::string extendedDictionary = curr + next;
+        if(dictionary.count(extendedDictionary)) {
+            curr = extendedDictionary;
         } else {
-            if(odd) {
-                packedArray.push_back(dictionary[w] & 0xFF);
-                packedArray.push_back((dictionary[w] >> 8) & 0xF);
-                odd = false;
+            if(even) {
+                packedArray.push_back(dictionary[curr] & 0xFF);
+                packedArray.push_back((dictionary[curr] >> 8) & 0xF);
+                even = false;
             } else {
-                packedArray.at(packedArray.back()) |= dictionary[w] << 4;
-                packedArray.push_back(dictionary[w] & 0xFF0);
-                odd = true;
+                packedArray.at(packedArray.back()) |= dictionary[curr] << 4;
+                packedArray.push_back(dictionary[curr] & 0xFF0);
+                even = true;
             }
-            dictionary[wc] = dictSize++;
-            w = c;
+            dictionary[extendedDictionary] = dictSize++;
+            curr = next;
         }
     }
 
-    if(!w.empty()) {
-        if(odd) {
-            packedArray.push_back(dictionary[w] & 0xFF);
-            packedArray.push_back((dictionary[w] >> 8) & 0xF);
-            odd = false;
+    if(!curr.empty()) {
+        if(even) {
+            packedArray.push_back(dictionary[curr] & 0xFF);
+            packedArray.push_back((dictionary[curr] >> 8) & 0xF);
+            even = false;
         } else {
-            packedArray.at(packedArray.back()) |= dictionary[w] << 4;
-            packedArray.push_back(dictionary[w] & 0xFF0);
-            odd = true;
+            packedArray.at(packedArray.back()) |= dictionary[curr] << 4;
+            packedArray.push_back(dictionary[curr] & 0xFF0);
+            even = true;
         }
     }
 }
 
 // need to redesign for 12-bit LSB
-void decode(std::string& input, std::vector<unsigned char> unpackedArray) {
+void decode(std::string& input, std::vector<unsigned char>& unpackedArray) {
     int dictSize = 256;
-    std::map<int,std::string> dictionary;
+    std::map<int16_t,std::string> dictionary; // only using 12 bits
     for(int i = 0; i < dictSize; i++) {
-        dictionary[i] = std::to_string(i);
+        dictionary[i] = std::string(1,i);
     }
 
-    std::string w = std::to_string(input.at(0));
+    std::string w(1,input.at(0)); // wtf is w
     std::string decodedString = w;
-    std::string entry;
+    std::string newEntry;
+    int sequences;
+    bool even=true;
     for(int i = 1; i < input.size(); i++) {
-        int k = input.at(i);
-        if(dictionary.count(k)) {
-            entry = dictionary[k];
-        } else if(k == dictSize) {
-            entry = w + w[0];
-        } else {
-            throw "String not found at k";
+        int16_t curr;
+        if(even) {
+            curr = input.at(i-1) + ((input.at(i) & 0xF) << 8);
+            even=false;
+        }
+        else {
+            curr = ((input.at(i-1) & 0xF0) >> 4) + ((input.at(i) & 0xF) << 8); // fix?
+            even=true;
+            i++;
         }
 
-        decodedString+=entry;
+        if(dictionary.count(curr)) {
+            newEntry = dictionary[curr];
+        } else if() {
 
-        dictionary[dictSize++] = w + entry[0];
-
-        w = entry;
+        }
     }
-
-    input.assign(decodedString);
 }
