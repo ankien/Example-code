6f4d3b5c641cdd5a98179da20671e52596bf0eb1
ankien <52425502+ankien@users.noreply.github.com>
2020-07-04 01:45:13 -0400

    Only works for text files.

--- lzw.h
+++ lzw.h
@@ -4,12 +4,12 @@
 #include<map>
 
 // Uses even/odd sequencing to store 12-bit codes in an 8-bit vector
-// Even: Index at 0, 2, 4, ...
-//  Odd: Index at 1, 3, 5, ...
+// Even: Index at [0-1), [3-4), [6-7), ...
+//  Odd: Index at (1-2], (4-5], (7-8], ...
 
 void encode(std::string& input, std::vector<unsigned char>& packedArray) {
-    int16_t dictSize = 256;
-    std::map<std::string,int16_t> dictionary; // only using 12 bits of int
+    uint16_t dictSize = 256;
+    std::map<std::string,uint16_t> dictionary; // only using 12 bits of int
     for(int i = 0; i < dictSize; i++) {
         dictionary[std::string(1,i)] = i;
     }
@@ -48,41 +48,48 @@ void encode(std::string& input, std::vector<unsigned char>& packedArray) {
 
 void decode(std::string& input, std::vector<unsigned char>& unpackedArray) {
     int dictSize = 256;
-    std::map<int16_t,std::string> dictionary; // only using 12 bits
+    std::map<uint16_t,std::string> dictionary; // only using 12 bits
     for(int i = 0; i < dictSize; i++) {
         dictionary[i] = std::string(1,i);
     }
     
-    int16_t curr;
-    int16_t next;
+    uint16_t curr;
+    uint16_t next;
     std::string newEntry;
+    std::string output;
     bool even=true;
     for(int i = 1; i < input.size(); i++) {
         if(even) {
             curr = input.at(i-1) + ((input.at(i) & 0xF) << 8);
-            newEntry = dictionary[curr];
-            // add curr + next to dictionary here, if there is no next, don't add to dictionary
+            output = dictionary[curr];
+            newEntry = dictionary[curr].at(0);
+            // add curr + next to dictionary here
             if((input.size() - i) > 1) {
                 next = ((input.at(i) & 0xF0) >> 4) + (input.at(i+1) << 4);
-                newEntry += dictionary[next];
-                dictionary[dictSize++] = newEntry;
+                newEntry = dictionary[curr];
+                newEntry += dictionary[next].at(0);
             }
+            dictionary[dictSize++] = newEntry;
             even=false;
         }
         else {
             curr = ((input.at(i-1) & 0xF0) >> 4) + (input.at(i) << 4);
-            newEntry = dictionary[curr];
+            output = dictionary[curr];
+            newEntry = dictionary[curr].at(0);
             // ditto for odd
             if((input.size() - i) > 2) {
                 next = input.at(i+1) + ((input.at(i+2) & 0xF) << 8);
-                newEntry += dictionary[next];
-                dictionary[dictSize++] = newEntry;
+                newEntry = dictionary[curr];
+                newEntry += dictionary[next].at(0);
             }
+            dictionary[dictSize++] = newEntry;
             even=true;
             i++;
         }
 
-        for(char character : newEntry)
+        for(char character : output)
             unpackedArray.push_back(character);
+
+        newEntry="";
     }
 }
